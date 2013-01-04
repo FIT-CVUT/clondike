94ba7e83f861954e5d6770f63c3853f4a5637e0c
Jiří Rákosník <rakosjir@fit.cvut.cz>
2013-01-04 17:48:45 +0100

    Oprava navodu

--- create_folder_structure.sh
+++ create_folder_structure.sh
@@ -3,12 +3,14 @@
 
 mkdir -p /clondike/ccn/mig
 cd /clondike/ccn
-touch listen stop-listen-one stop-listen-all stop listening-on nodes
+mkdir nodes
+touch listen stop-listen-one stop-listen-all stop listening-on
 cd mig
 touch migrate-home emigrate-ppm-p migproc
 mkdir -p /clondike/pen/mig
 cd /clondike/pen
-touch listen stop-listen-one stop-listen-all stop listening-on nodes
+mkdir nodes
+touch listen stop-listen-one stop-listen-all stop listening-on
 cd mig
 touch migrate-home emigrate-ppm-p migproc
 
