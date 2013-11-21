a42df5d10a16ada11d4046562a3cdc3ec809299f
shaddeus <pawel.tvrdik@gmail.com>
2013-11-21 07:59:51 -0600

    Fix parallel util script

--- scripts/devel/cluster/scripts/update_git_and_restart_clondike.sh
+++ scripts/devel/cluster/scripts/update_git_and_restart_clondike.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-$PROXY_USER=
-$PROXY_PASSWORD=
+#
+#  Will update GIT repository and restart Clondike
+#
+#  IMPORTANT: If you need proxy setting, you must fill below proxy access data!
+#
 
-PROXY="192.168.88.1:3128"
-export HTTP_PROXY="http://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
-export http_proxy="http://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
-export HTTPS_PROXY="https://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
-export https_proxy="https://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
+PROXY_USER=
+PROXY_PASSWORD=
+
+if [ -n "$PROXY_USER" -a -n "$PROXY_PASSWORD" ]; then
+	PROXY="192.168.88.1:3128"
+	export HTTP_PROXY="http://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
+	export http_proxy="http://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
+	export HTTPS_PROXY="https://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
+	export https_proxy="https://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY}"
+fi
 
 dhclient eth0
 
