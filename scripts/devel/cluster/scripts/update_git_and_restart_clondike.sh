8e54f6aac31ec62a1380be009af4c2bd123544a2
shaddeus <pawel.tvrdik@gmail.com>
2013-11-21 13:39:24 -0600

    Fix update git and restart clondike script

--- scripts/devel/cluster/scripts/update_git_and_restart_clondike.sh
+++ scripts/devel/cluster/scripts/update_git_and_restart_clondike.sh
@@ -20,8 +20,14 @@ if [ -n "$PROXY_USER" -a -n "$PROXY_PASSWORD" ]; then
 fi
 
 dhclient eth0
+DHCLIENT_PID=$!
 
 cd /root/clondike
 git fetch origin
 git reset --hard origin/master
+
+kill $DHCLIENT_PID
+IP=`ifconfig eth0  | awk '{print $2}' | sed -n '2p' | cut -d ":" -f2`
+ip address del ${IP}/24 dev eth0
+
 /root/clondike/scripts/restart-director.sh
