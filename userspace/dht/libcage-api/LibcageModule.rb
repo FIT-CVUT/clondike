3b620c55b6f30b1fc451a65e7496174d13b26cb2
shaddeus <pawel.tvrdik@gmail.com>
2013-11-23 05:38:12 -0600

    Improving clondike-init and add VIM editor stuff.

--- userspace/dht/libcage-api/LibcageModule.rb
+++ userspace/dht/libcage-api/LibcageModule.rb
@@ -1,17 +1,17 @@
 require 'ffi'
 
 module LibcageModule
-	extend FFI::Library
+  extend FFI::Library
 
-	begin
-		ffi_lib [File.dirname(__FILE__)+'/libcage-api.so']
-	rescue => LoadError
-		puts "#{LoadError.backtrace.join("\n")}"
-		raise 'Failed to load Libcage Library!'
-	end
+  begin
+    ffi_lib [File.dirname(__FILE__)+'/libcage-api.so']
+  rescue => LoadError
+    puts "#{LoadError.backtrace.join("\n")}"
+    raise 'Failed to load Libcage Library!'
+  end
 
-	attach_function :libcage_open,          [:int],                     :pointer
-	attach_function :libcage_print_state,   [:pointer],                 :void
-	attach_function :libcage_join,          [:pointer, :string, :int],  :bool
-	attach_function :libcage_dispatch,      [],                         :void
+  attach_function :libcage_open,          [:int],                     :pointer
+  attach_function :libcage_print_state,   [:pointer],                 :void
+  attach_function :libcage_join,          [:pointer, :string, :int],  :bool
+  attach_function :libcage_dispatch,      [],                         :void
 end
