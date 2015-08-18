229da034016eae8b8ce8ff013ef86426c8d455d1
Zdenek Novy <novyzde3@fit.cvut.cz>
2015-08-18 16:15:22 +0200

    Add userspace configuration
    Add ruby configuration mechanism to userspace from one config file.

---
+++ userspace/simple-ruby-director/Configuration.rb
@@ -0,0 +1,156 @@
+#!/usr/bin/ruby -w
+
+require 'pathname'
+
+
+##################### CLASS CONFIGURATION ####################
+#
+# Class to parse Clondike config file, save the configuration
+#  directives to a hash table and return a wanted value 
+#  of the directive
+# Author: Zdenek Novy (novyzde3@fit.cvut.cz)
+#
+#################### CLASS CONFIGURATION ####################
+class Configuration
+################## Constants ###################
+   REGEX_CHAR               = '[A-Za-z0-9_-]'
+   REGEX_DIRECTIVE          = '\s*(' + REGEX_CHAR + '+)\s*'
+   REGEX_COMMENT            = '\s*#.*$'
+   REGEX_LINEOFWHITESPACES  = '^\s*$'
+   REGEX_SPACEANDTAB        = '[ \t]'
+   DEBUG                    = true
+   #################################################
+
+   ################ Class variables ###############
+   # Config file where is stored the configuration
+	@@configFile = nil
+
+	# Hash key => value table
+	#   with directives and their values
+   @@ya_directives = nil
+   #################################################
+
+
+   ############ Methods ##############
+   # Display error message and exit the program
+   # @param y_msg string
+   def error( y_msg, i_err=-1 )
+       $log.error( "[ERROR]\t(Configuration.rb): #{y_msg}!" )
+       exit( i_err )
+   end
+ 
+   # Create an object Configuration and load
+   #  config file to memory
+   # @param string   name of config file to load
+   def initialize( configFile )
+      @@ya_directives = Hash.new
+
+      # Check file existence
+      config = Pathname.new(configFile)
+      if not config.exist?
+ 	      error("config file #{configFile} does not exist", 1)
+      end
+      
+      @@configFile=configFile
+      if ( ! loadConfigFile() )
+         error("Log file #{@@configFile} can not be loaded", 1)
+      end
+   end
+
+   # Check config file and parse them into has table
+   def loadConfigFile( )
+      if @@configFile == nil
+         error("config file #{@@configFile} is not inicialized")
+      end
+
+      ### Reading file in a cycle
+      File.foreach( @@configFile ).with_index do |y_line, i_line|
+         i_line+=1
+
+         # Remove comments ('whitespaces # anything')
+         y_line=y_line.sub(/#{REGEX_COMMENT}/, '')
+
+         # Remove newline symbol from the line
+         y_line=y_line.chomp
+
+			# Skip line with whitespaces only
+         if y_line =~ /#{REGEX_LINEOFWHITESPACES}/
+            next
+         end
+
+         ### Check syntax if wrong - exit ###
+         # value in quotation marks ( "value" )
+         if y_line.include? '"'
+            if y_line =~ /^#{REGEX_DIRECTIVE}=#{REGEX_SPACEANDTAB}*".+"#{REGEX_SPACEANDTAB}*$/
+               ya_line=y_line.split('=')
+               y_value=ya_line[1].gsub(/^#{REGEX_SPACEANDTAB}*"(.+)"#{REGEX_SPACEANDTAB}*$/, '\\1')
+            else
+               error("config file '" + @@configFile + "' has bad syntax on line "+i_line.to_s, 10)
+            end
+
+         # value in apostrophes ( 'value' )
+		   elsif y_line.include? "'"
+            if y_line =~ /^#{REGEX_DIRECTIVE}=#{REGEX_SPACEANDTAB}*'.+'#{REGEX_SPACEANDTAB}*$/
+               ya_line=y_line.split('=')
+               y_value=ya_line[1].gsub(/^#{REGEX_SPACEANDTAB}*'(.+)'#{REGEX_SPACEANDTAB}*$/, '\\1')
+            else
+               error("Config file '" + @@configFile + "' has bad syntax on line "+i_line.to_s, 11)
+            end
+
+         # value is without special characters ( value )
+         else
+            if y_line =~ /^#{REGEX_DIRECTIVE}=#{REGEX_SPACEANDTAB}*#{REGEX_CHAR}+#{REGEX_SPACEANDTAB}*$/
+               ya_line=y_line.split('=')
+               y_value=ya_line[1].gsub(/^#{REGEX_SPACEANDTAB}*(#{REGEX_CHAR}+)#{REGEX_SPACEANDTAB}*$/, '\\1')
+            else
+               error("Config file '" + @@configFile + "' has bad syntax on line "+i_line.to_s, 12)
+            end
+         end
+
+         ### Parse the directive
+         y_directive=ya_line[0].gsub(/^#{REGEX_DIRECTIVE}$/, '\\1')
+
+         ### Set value to hash table
+         @@ya_directives[y_directive] = y_value
+      end
+      return true
+   end
+
+   ### Get the value of wanted directive
+   # @param    string   name of the wanted directive
+   # @return   string   value of the directive or null if not exists
+   def getValue(directive)
+      if @@ya_directives.empty?
+         error("Error: ya_directives si null", 21)
+      end
+      if DEBUG
+          if @@ya_directives[directive].nil?
+              $log.warn( "[WARNING]\tReturning directive '#{directive}' with null value" )
+          else
+              $log.debug( "[OK]\tReturning directive '#{directive}' with value '" + @@ya_directives[directive] + "'" )
+          end
+      end
+      return @@ya_directives[directive]
+   end
+
+   # Method to print actual Hash table
+   #  with actually loaded directives and their value
+   def printDirectives()
+      puts "\n[DEBUG]\tPrint directives:"
+      @@ya_directives.each do |key, value|
+         puts key.to_s + ':  \'' + value + '\''
+      end
+      print "\n"
+   end
+
+end
+################# END OF CLASS CONFIGURATION #################
+
+
+### TESTING THE CLASS ###
+#configuration = Configuration.new("clondike.conf")
+#configuration.printDirectives()
+#puts "getValue(): '" + configuration.getValue('interface') + "'"
+
+
+
---
+++ userspace/simple-ruby-director/clondike-start.rb
@@ -0,0 +1,129 @@
+#!/usr/bin/ruby -w
+
+require 'Configuration.rb'
+require 'Director.rb'
+require 'resolv'
+require 'logger'
+require 'fileutils'
+
+class Clondike
+    @localIP = nil
+
+    ###################
+    def getConfigDirective( configuration, directive, default_value )
+        value = configuration.getValue( directive )
+        value = default_value if value==nil
+        return value
+    end
+
+    ###################
+    def getLocalIP( configuration )
+        interface   = getConfigDirective( configuration, 'interface', 'eth0' )
+        command     = "ifconfig | awk '/#{interface}/ { getline; adrs=$2; split(adrs,adr,\":\"); printf \"%s\", adr[2]; }'"
+        localIP     = `#{command}`
+
+        # test IP address and if not pass exit with error
+        if ( localIP !~ Resolv::IPv4::Regex )
+            $log.error( "[ERROR]\tIP address #{localIP} of interface #{interface} was not found or is wrong! Please check the interface.\n" )
+            exit 1
+        end
+        return localIP
+    end
+
+    ###################
+    def clearCurrentConfig()
+        $log.debug( "[..]\tClearing current config" )
+        FileUtils.rm_rf( Dir.glob('conf/*') )
+        $log.debug( "[OK]\tDeleting current config complete" )
+        
+        # README file
+        File.write( 'conf/README', 'This directory should be empty before starting Clondike' )
+
+        $log.debug( "[OK]\tClearing current config complete" )
+    end
+
+    ###################
+    # Mount 9p and proxyfs filesystems
+    def mount( configuration, listen_ip )
+        # 9p mounting parameters
+        $log.debug( "[..]\tCreating mounting files" )
+        mounterdir = getConfigDirective( configuration, 'mounterdir', '/clondike/ccn/mounter/' )
+        File.write( "#{mounterdir}fs-mount", '9p-global' )
+        File.write( "#{mounterdir}fs-mount-device", listen_ip )
+        File.write( "#{mounterdir}fs-mount-options", 'aname=/,uname=root,port=5577' )
+        $log.debug( "[OK]\tMounting files created in #{mounterdir}" )
+
+        # Proxyfs
+        command = "mount -t proxyfs tcp:#{listen_ip}:1112 /mnt/proxy"
+        `#{command}`
+    end
+
+    ###################
+    def listen( configuration )
+        @localIP = getLocalIP( configuration ) if @localIP==nil
+        listen_ip   = @localIP
+        listen_port = getConfigDirective( configuration, 'listenport', '54321' )
+        listen_prot = getConfigDirective( configuration, 'listenprot', 'tcp'   )
+        listen_str  = "#{listen_prot}:#{listen_ip}:#{listen_port}"
+        $log.debug( "[INFO]\tListenstr: #{listen_str}" )
+
+        # Mount 9p and proxyfs filesystems
+        mount( configuration, listen_ip )
+
+        # Listen file
+        listen_file = getConfigDirective( configuration, 'listenfile', '/clondike/ccn/listen' )
+        if not File.file?( listen_file )
+            $log.error( "[ERROR]\tCan't listen on '#{listen_str}', missing listen file '#{listen_file}'" )
+            exit 1
+        end
+        # File.write( listen_file, listen_str ) # Can't write by ruby, have to write by shell
+        command = "echo #{listen_str} > #{listen_file}"
+        `#{command}`
+        $log.info( "[OK]\tStarting listening on #{listen_str}.." )
+        $log.debug( "[OK]\tListening complete" )
+    end
+
+    ###################
+    def initialize
+        configuration = Configuration.new('clondike.conf')
+        @localIP = getLocalIP( configuration );
+
+        clearCurrentConfig()
+
+
+        listen( configuration )
+        #cmd = "../../scripts/listen.sh #{ip}"
+        #$log.debug( "#{cmd}" )
+        #`#{cmd}`
+
+       $log.debug( "[OK]\tStarting Clondike complete..\n.................................\n\n" )
+    end
+end
+
+########### Main program #############
+begin
+    $log = Logger.new(STDOUT)
+    $log.level = Logger::INFO;
+    $log.datetime_format = "%Y-%m-%d %H:%M:%S"
+
+    $useProcTrace = false
+
+    clondike = Clondike.new()
+
+    # Running director
+    director = Director.new()
+    $log.debug( "[OK]\tDirector created" )
+    director.start()
+    $log.debug( "[OK]\tDirector started" )
+    director.waitForFinished()
+    $log.debug( "[OK]\tDirector waiting complete" )
+
+rescue => err
+    $log.error "Error in main Director thread: #{err.message} \n#{err.backtrace.join("\n")}"
+end
+
+
+
+
+
+
