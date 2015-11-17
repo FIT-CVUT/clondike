d3903ed3c15ca41347d2aac8bd0492cbfd0badf3
Zdenek Novy <novyzde3@fit.cvut.cz>
2015-11-17 12:14:47 +0100

    Check empty msg-seq file and edit log
    Edit log format for Tasks to special log.
    Added check for existing but empty file msg-seq.

--- userspace/simple-ruby-director/ExecutionTimeTracer.rb
+++ userspace/simple-ruby-director/ExecutionTimeTracer.rb
@@ -1,11 +1,26 @@
+require 'logger'
+
 # Listener pluggable to TaskRepository, that can trace execution times of individual tasks
 class ExecutionTimeTracer
   def newTask(task)
     # Nothing to be done
-  end
+  end 
 
   def taskExit(task, exitCode)
+    $taskLog = Logger.new('/var/log/clondike-taskLog')
+    $taskLog.level = Logger::DEBUG
+    $taskLog.datetime_format = "%Y-%m-%d %H:%M:%S"
+    
+    #$taskLog.info("------## Class: #{task.class}")
+
     endTime = Time.now.to_f
-    puts  "Task #{task.name} (#{task.pid}) was executed in #{endTime-task.startTime} seconds. Exit code: #{exitCode}. Execution node: #{task.executionNode}"
-  end
+    $taskLog.debug("
+    Task: #{task.name} (#{task.pid})
+        #{task.args.join(' ')}
+    Duration:       #{endTime-task.startTime} seconds.
+    Exit code:      #{exitCode}.
+    Execution node: #{task.executionNode}
+    ")
+  end 
 end
+
