8ff127a15f29f3520a5130f4cf558fca5a5cb449
Zdenek Novy <novyzde3@fit.cvut.cz>
2015-11-18 11:19:37 +0100

    Modified init script and logging
    Change clondike init script to start after interfaces are up and small changes to logging.

--- userspace/simple-ruby-director/ExecutionTimeTracer.rb
+++ userspace/simple-ruby-director/ExecutionTimeTracer.rb
@@ -3,23 +3,20 @@ require 'logger'
 # Listener pluggable to TaskRepository, that can trace execution times of individual tasks
 class ExecutionTimeTracer
   def newTask(task)
-    # Nothing to be done
-  end 
-
-  def taskExit(task, exitCode)
+    # Initializing special log for tasks
     $taskLog = Logger.new('/var/log/clondike-taskLog')
     $taskLog.level = Logger::DEBUG
     $taskLog.datetime_format = "%Y-%m-%d %H:%M:%S"
-    
-    #$taskLog.info("------## Class: #{task.class}")
+  end
 
+  def taskExit(task, exitCode)
     endTime = Time.now.to_f
     $taskLog.debug("
-    Task: #{task.name} (#{task.pid})
-        #{task.args.join(' ')}
-    Duration:       #{endTime-task.startTime} seconds.
-    Exit code:      #{exitCode}.
-    Execution node: #{task.executionNode}
+        Task: #{task.name} (#{task.pid})
+            #{task.args.join(' ')}
+        Duration:       #{endTime-task.startTime} seconds.
+        Exit code:      #{exitCode}.
+        Execution node: #{task.executionNode}
     ")
   end 
 end
