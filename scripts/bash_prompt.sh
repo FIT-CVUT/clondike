13fc55381fa88909d059918c5b7b059f39756a6a
Zdenek Novy <novyzde3@fit.cvut.cz>
2015-11-19 11:22:03 +0100

    Edited init scripts and bash prompt
    Edited init scripts for Clondike and new NPFS init script. Edit bash prompt to show result of last command.

--- scripts/bash_prompt.sh
+++ scripts/bash_prompt.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 # Script for set Clondike prompt
 # Autor: Jiri Rakosnik
+#           edited by Zdenek Novy
+
+# Save result value of the last command
+CC=$?
+
+# Function calling by every command to color last sharp
+#  by return code of the last command
+if [ "$1" = "defPS1" ] ; then
+    [ $CC -eq 0 ] && DLR="\[\e[33;1m\]" || DLR="\[\e[31;1m\]"
+	if [[ ${EUID} == 0 && -d /clondike/pen/nodes && -d /clondike/ccn/nodes ]] ; then
+        PS1="\[\033[01;31m\]$(cut -d: -f2 /clondike/ccn/listen) $(cat /clondike/pen/nodes/count)/$(cat /clondike/ccn/nodes/count)\[\033[01;34m\] \W $DLR\\$\[\033[00m\] "
+	else
+		PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w $DLR\\$\[\033[00m\] '
+	fi
+    return 0;
+fi
+
+
 
 # Change the window title of X terminals 
 case ${TERM} in
@@ -28,8 +46,7 @@ match_lhs=""
 	&& match_lhs=$(dircolors --print-database)
 [[ $'\n'${match_lhs} == *$'\n'"TERM "${safe_term}* ]] && use_color=true
 
-if ${use_color}
-then
+if ${use_color} ; then
 	# Enable colors for ls, etc.  Prefer ~/.dir_colors #64489
 	if type -P dircolors >/dev/null ; then
 		if [[ -f ~/.dir_colors ]] ; then
@@ -38,12 +55,9 @@ then
 			eval $(dircolors -b /etc/DIR_COLORS)
 		fi
 	fi
-	if [[ ${EUID} == 0 && -d /clondike/pen/nodes && -d /clondike/ccn/nodes ]]
-	then
-		PS1='\[\033[01;31m\]$(cut -d: -f2 /clondike/ccn/listen) $(cat /clondike/pen/nodes/count)/$(cat /clondike/ccn/nodes/count)\[\033[01;34m\] \W \$\[\033[00m\] '
-	else
-		PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
-	fi
+    PROMPT_COMMAND="source ${BASH_SOURCE[0]} defPS1"
+    # Old way without colors sharp by result of the last command + if from function defPS1
+    #PS1='\[\033[01;31m\]$(cut -d: -f2 /clondike/ccn/listen) $(paste -d/ /clondike/{pen,ccn}/nodes/count)\[\033[01;34m\] \W \$\[\033[00m\] '
 
 	alias ls='ls --color=auto'
 	alias grep='grep --colour=auto'
