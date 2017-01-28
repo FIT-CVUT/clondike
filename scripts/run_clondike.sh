23d659d09d1c1cdd5f4ca0b539345814d4a86901
Josef Gattermayer <pepus@Josef-iMac.local>
2017-01-28 22:25:47 +0100

    removed cassandra

--- scripts/run_clondike.sh
+++ scripts/run_clondike.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CASSANDRA_IP=$1
+BIGCHAIN_URL=$1
 BOOTSTRAP_IP=$2
 IS_DAEMON=$3
 
-echo ${CASSANDRA_IP}
+echo ${BIGCHAIN_URL}
 echo ${BOOTSTRAP_IP}
 
-sed -i "s/^\(cassandra_hosts='\).*\('.*\)/\1$CASSANDRA_IP\2/" /root/clondike/userspace/simple-ruby-director/clondike.conf
+sed -i "s/^\(bigchain_endpoint='\).*\('.*\)/\1$BIGCHAIN_URL\2/" /root/clondike/userspace/simple-ruby-director/clondike.conf
 sed -i "s/^\(bootstrap='\).*\('.*\)/\1$BOOTSTRAP_IP\2/" /root/clondike/userspace/simple-ruby-director/clondike.conf
 
 /root/clondike/kernel_simulator/clondike_kernel_simulator > /tmp/simulator.log 2> /tmp/simulator.err &
