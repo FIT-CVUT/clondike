8686b0f581966f6d3a47c7924cdea4b234be3b11
Josef Gattermayer <josef.gattermayer@ackee.cz>
2017-03-27 21:46:11 +0200

    rozdelany keyvalue

--- userspace/blockchain/accept_task.py
+++ userspace/blockchain/accept_task.py
@@ -2,22 +2,30 @@ import sys
 import random
 from simplekv.fs import FilesystemStore
 import kudos
+import logging
+import base64
 
 def main(argv):
+	logging.basicConfig(filename='/tmp/kudos.log',level=logging.INFO)
 	localKey = argv[1]
 	remoteKey = argv[2]
 	alice_verifying_key, alice_signing_key = kudos.getMyKeys()
-	db = FilesystemStore('keyfiles')
-	try:
-		db.get(alice_verifying_key)
-	except KeyError as e:
-		db.put(alice_verifying_key, str.encode(localKey))
-		print("set :", alice_verifying_key, localKey)
-	try:
-		db.get(localKey)
-	except KeyError as e:
-		db.put(localKey, str.encode(alice_verifying_key))
-		print("set :", localKey, alice_verifying_key)
+	db = FilesystemStore('/tmp')
+	#try:
+	#	db.get(alice_verifying_key)
+	#except KeyError as e:
+	logging.info(alice_verifying_key, localKey)
+	db.put(str(base64.b64encode(str.encode(alice_verifying_key))), str.encode(localKey))
+	#	print("set :", alice_verifying_key, localKey)
+	#try:
+	#	db.get(localKey)
+	#except KeyError as e:
+	db.put(str(base64.b64encode(str.encode(localKey))), str.encode(alice_verifying_key))
+	#	print("set :", localKey, alice_verifying_key)
+	a=db.get(str(base64.b64encode(str.encode(localKey))))
+	print(a.decode())
+	b=db.get(str(base64.b64encode(str.encode(alice_verifying_key))))
+	print(b.decode())
 	sys.exit(0)
 
 def decision(probability):
--- userspace/simple-ruby-director/LimitersImmigrationController.rb
+++ userspace/simple-ruby-director/LimitersImmigrationController.rb
@@ -6,7 +6,10 @@ class LimitersImmigrationController
   end
 
   def onImmigrationRequest(node, execName, localKey, remoteKey)
-    cmd = `python clondike/userspace/blockchain/accept_task.py "#{localKey}" "#{remoteKey}"`
+    cmd = `pwd`
+    #$log.info("Path is #{cmd}")
+    cmd = `python3.5 clondike/userspace/blockchain/accept_task.py "#{localKey}" "#{remoteKey}"`
+    $log.info("python3.5 clondike/userspace/blockchain/accept_task.py #{localKey} #{remoteKey}")
     $log.info("Python returns #{$?.success?}")
     #RANDOM
     if $?.success?
